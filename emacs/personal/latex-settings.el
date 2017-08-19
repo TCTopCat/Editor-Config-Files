1a86446f37868aa66f150a73653d565984aefb45
TCTopCat <tc.nogginthenog@gmail.com>
2017-08-19 15:28:21 +0100

    Took out the argument from the emacs alias that focuses the frame as it's incompatible with adding filename arguments. In the process, also converted the alias to a bash function.

--- emacs/personal/latex-settings.el
+++ emacs/personal/latex-settings.el
@@ -8,26 +8,26 @@
 
 (add-hook 'LaTeX-mode-hook 'set-buffer-olivetti-variable-pitch)
 
-(use-package ox
-  :defer t
-  :config
-  ;; ** <<APS journals>>
-  (add-to-list 'org-latex-classes '("revtex4-1"
-									"\\documentclass[aps,prl,twocolumn]{revtex4-1}
-																	[NO-DEFAULT-PACKAGES] [PACKAGES] [EXTRA]"
-									("\\section{%s}" . "\\section*{%s}")
-									("\\subsection{%s}" . "\\subsection*{%s}")
-									("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-									("\\paragraph{%s}" . "\\paragraph*{%s}")
-									("\\subparagraph{%s}" .
-									 "\\subparagraph*{%s}")))
-  (add-to-list 'org-latex-classes '("nature"
-									"\\documentclass[fleqn,10pt]{wlscirep}
-																	[NO-DEFAULT-PACKAGES] [PACKAGES] [EXTRA]"
-									("\\section{%s}" . "\\section*{%s}")
-									("\\subsection{%s}" . "\\subsection*{%s}")
-									("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-									("\\paragraph{%s}" . "\\paragraph*{%s}")
-									("\\subparagraph{%s}" .
-									 "\\subparagraph*{%s}")))
-  )
+;; (use-package ox
+;;   :defer t
+;;   :config
+;;   ;; ** <<APS journals>>
+;;   (add-to-list 'org-latex-classes '("revtex4-1"
+;; 									"\\documentclass[aps,prl,twocolumn]{revtex4-1}
+;; 																	[NO-DEFAULT-PACKAGES] [PACKAGES] [EXTRA]"
+;; 									("\\section{%s}" . "\\section*{%s}")
+;; 									("\\subsection{%s}" . "\\subsection*{%s}")
+;; 									("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+;; 									("\\paragraph{%s}" . "\\paragraph*{%s}")
+;; 									("\\subparagraph{%s}" .
+;; 									 "\\subparagraph*{%s}")))
+;;   (add-to-list 'org-latex-classes '("nature"
+;; 									"\\documentclass[fleqn,10pt]{wlscirep}
+;; 																	[NO-DEFAULT-PACKAGES] [PACKAGES] [EXTRA]"
+;; 									("\\section{%s}" . "\\section*{%s}")
+;; 									("\\subsection{%s}" . "\\subsection*{%s}")
+;; 									("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+;; 									("\\paragraph{%s}" . "\\paragraph*{%s}")
+;; 									("\\subparagraph{%s}" .
+;; 									 "\\subparagraph*{%s}")))
+;;   )
